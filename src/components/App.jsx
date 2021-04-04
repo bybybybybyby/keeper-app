298ae4337819dd34b1736fba5d1fa7bb003493d5
Brian <bybyby@gmail.com>
2021-04-03 20:46:53 -0700

    Added functionality to add note and delete notes.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
-      <Footer />
-    </div>
-  );
+
+    const [notes, setNotes] = useState([{ title: "Groceries", content: "Buy apples and bananers" }]);
+
+    function addNote(title, note) {
+        setNotes(prevNotes => {
+            return (
+                [...prevNotes, { title: title, content: note }]
+            )
+        })
+        event.preventDefault();  // Prevent page refresh
+    }
+
+    function deleteNote(id) {
+        setNotes(prevNotes => {
+            return prevNotes.filter((note, index) => {
+                return (index !== id)
+            })
+        })
+    }
+
+    return (
+        <div>
+            <Header />
+            <CreateArea handleClick={addNote} />
+            {notes.map((note, idx) => {
+                return <Note
+                    key={idx}
+                    id={idx}
+                    title={note.title}
+                    content={note.content}
+                    onDelete={deleteNote} />
+            })}
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [title, setTitle] = useState("");
+  const [noteText, setNoteText] = useState("");
+
+  function handleTitle(event) {
+    setTitle(event.target.value)
+  }
+
+  function handleNote(event) {
+    setNoteText(event.target.value)
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleTitle} name="title" placeholder="Title" value={title} />
+        <textarea onChange={handleNote} value={noteText} name="content" placeholder="Take a note..." rows="3" />
+        <button onClick={() => {
+          props.handleClick(title, noteText);
+          setTitle("");
+          setNoteText("");
+        }}>Add</button>
       </form>
+
     </div>
   );
 }
