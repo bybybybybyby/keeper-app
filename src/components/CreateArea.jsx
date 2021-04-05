e5f54e385f13d4e3056572d04756bcf99da7fa98
Brian <bybyby@gmail.com>
2021-04-05 14:20:38 -0700

    Use Material-UI design for expandable new note and button design

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 
 function CreateArea(props) {
   const [note, setNote] = useState({
     title: "",
     content: ""
   });
+
+  const [expandedNote, setExpandedNote] = useState({
+    isHidden: "hidden",
+    numRows: 1,
+    zoom: false
+  })
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -22,15 +31,41 @@ function CreateArea(props) {
       props.onAdd(note);
       setNote({title:"", content:""});
     }
+    setExpandedNote({
+      isHidden: "hidden",
+      numRows: 1,
+      zoom: false
+    })
     event.preventDefault();  // Prevent page refresh
+  }
+
+  function expandNote() {
+    setExpandedNote({
+      isHidden: "",
+      numRows: 3,
+      zoom: true
+    })
   }
 
   return (
     <div>
       <form className="create-note">
-        <input onChange={handleChange} name="title" value={note.title} placeholder="Title" />
-        <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
-        <button onClick={submitNote}>Add</button>
+        <input 
+          type={expandedNote.isHidden}
+          onChange={handleChange} 
+          name="title" 
+          value={note.title} 
+          placeholder="Title" />
+        <textarea 
+          onChange={handleChange} 
+          onClick={expandNote}
+          name="content" 
+          value={note.content} 
+          placeholder="Take a note..." 
+          rows={expandedNote.numRows} />
+        <Zoom in={expandedNote.zoom}>
+          <Fab onClick={submitNote}><AddIcon /></Fab>
+        </Zoom>
       </form>
     </div>
   );
