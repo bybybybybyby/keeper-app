846a72e2eb97031e43fd848a9775dbfed051b78b
Brian <bybyby@gmail.com>
2021-04-05 15:07:54 -0700

    Add transparent background.  Fix footer position.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 
 function Header() {
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1><MenuBookIcon /> Keeper</h1>
     </header>
   );
 }
